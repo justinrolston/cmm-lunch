d8914da64f41754d7bc825dc53f181a080595de5
Justin Rolston <justinrolston@gmail.com>
2013-03-28 07:33:18 -0400

    refactored condition to a methed

--- lib/cmm-lunch.rb
+++ lib/cmm-lunch.rb
@@ -5,11 +5,16 @@ class CmmLunch
     lines =  rawmenu.split("\n\n")
     menu = {}
     lines.each do |line|
-        if Date::DAYNAMES.any?{|d|  line.split("\n")[0].include? d}
-          item = line.split("\n")
-          menu[item[0].strip.to_sym] = [item[1],item[2]]
-        end
+      if lunch_line?(line)
+        item = line.split("\n")
+        menu[item[0].strip.to_sym] = [item[1],item[2]]
+      end
     end
     return menu
+  end
+
+  private
+  def self.lunch_line?(entry_line)
+    return Date::DAYNAMES.any?{|d|  entry_line.split("\n")[0].include? d}
   end
 end
